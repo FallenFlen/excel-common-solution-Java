28cda2e4e46eddccc5156d2eea7abdc360bd87f8
Fallenz <beijuspsn@163.com>
2023-07-17 15:17:08 +0800

    init

---
+++ src/main/java/com/flz/excel/solution/ExcelCommonSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.flz.excel.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelCommonSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelCommonSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flz/excel/solution/ExcelCommonSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flz.excel.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelCommonSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
