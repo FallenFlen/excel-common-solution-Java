3a312aabcf2dbc92ae8afc9d6ca649d7b71ce15f
Fallenz <beijuspsn@163.com>
2023-07-18 17:50:10 +0800

    add validation on DTO

--- src/main/java/com/flz/excel/solution/dto/skuimport/SkuImportDTO.java
+++ src/main/java/com/flz/excel/solution/dto/skuimport/SkuImportDTO.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 
 @AllArgsConstructor
@@ -12,10 +15,17 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class SkuImportDTO {
+    @NotBlank(message = "Sku编码不能为空")
     private String code;
+    @NotBlank(message = "Sku编码不能为空")
     private String spuCode;
+    @NotBlank(message = "Sku编码不能为空")
     private String name;
+    @NotNull(message = "价格不能为空")
+    @Positive(message = "价格必须为正数")
     private BigDecimal price;
+    @NotNull(message = "数量不能为空")
+    @Positive(message = "数量必须为正数")
     private Integer count;
     private String storageId;
 }
