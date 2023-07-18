7e71c3f30dbc715437b12dbfe00e03b088bda123
Fallenz <beijuspsn@163.com>
2023-07-18 18:17:58 +0800

    add log

--- src/main/java/com/flz/excel/solution/common/config/GlobalExceptionHandler.java
+++ src/main/java/com/flz/excel/solution/common/config/GlobalExceptionHandler.java
@@ -2,25 +2,27 @@ package com.flz.excel.solution.common.config;
 
 import com.flz.excel.solution.dto.common.ErrorResult;
 import com.flz.excel.solution.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler
+    @ExceptionHandler(BusinessException.class)
     public ErrorResult handleBusinessException(BusinessException e) {
-        e.printStackTrace();
+        log.error("BusinessException:", e);
         return ErrorResult.of(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
+    @ExceptionHandler(Exception.class)
     public ErrorResult handleOtherException(Exception e) {
-        e.printStackTrace();
+        log.error("Exception:", e);
         return ErrorResult.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
     }
 }
